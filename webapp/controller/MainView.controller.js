31657731ca4869cb6523b42f36d5496437bf8b5b
ggrabois_TEIC <ggrabois@proveedores.techint.com>
2026-05-02 20:21:40 +0000

    first commit on development2

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -5,6 +5,23 @@ sap.ui.define([
 
     return Controller.extend("gabrieldev.invoices.controller.MainView", {
         onInit() {
+
+            const oJSONModel = new sap.ui.model.json.JSONModel();
+            const oView = this.getView();
+            oJSONModel.loadData("./model/SelectionScreenMenu.json");
+            oView.setModel(oJSONModel, "selectionScreen");            
+
+        },
+        onFilter: function (oEvent) {
+
+        },
+
+        onClearFilter() {
+            const oModelSelScreen = this.getView().getModel("selectionScreen");
+            oModelSelScreen.setProperty("/ShipName", "");
+            oModelSelScreen.setProperty("/CountryKey", "");
+
         }
+
     });
 });
